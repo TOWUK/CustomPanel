db7680fabb47eff09736f7f2391607f681dd86ef
TOWUK <towuk@internet.ru>
2026-06-13 23:45:55 +0300

    я заебался босс, работает как швейцарский нож!

--- src/CustomPanel.java
+++ src/CustomPanel.java
@@ -1,6 +1,5 @@
 import arc.Events;
 import arc.util.Strings;
-import arc.util.Time;
 import arc.util.Timer;
 import mindustry.Vars;
 import mindustry.core.GameState;
@@ -16,18 +15,14 @@ public class CustomPanel extends Plugin {
     private static Timer.Task timerTask;
     @Override
     public void init() {
-        Events.on(EventType.WorldLoadEvent.class, e -> Time.runTask(2f, () -> {
-            if (Vars.state.map != null) mapName = Strings.stripColors(Vars.state.map.name());
-            update();
-        }));
-        Events.on(EventType.PlayerJoin.class, e -> { if (Groups.player.size() == 1) update(); });
+        Events.on(EventType.PlayEvent.class, e -> { mapName = Vars.state.map == null ? "" : Strings.stripColors(Vars.state.map.name()); update(); });
+        Events.on(EventType.PlayerJoin.class, e -> update());
         if (timerTask != null) timerTask.cancel();
-        float delay = (60f - LocalDateTime.now().getSecond()) % 60f + 0.1f;
-        timerTask = Timer.schedule(CustomPanel::update, delay, 60f);
+        timerTask = Timer.schedule(CustomPanel::update, (60f - LocalDateTime.now().getSecond()) % 60f + 0.1f, 60f);
     }
     private static void update() {
-        if (mapName.isEmpty() || Vars.state.is(GameState.State.menu) || Groups.player.isEmpty()) return;
+        if (mapName.isEmpty() || Vars.state.is(GameState.State.menu)) return;
         Vars.state.rules.mission = "[#dadada]Map:\n" + mapName + "\nTime: " + LocalDateTime.now().format(F);
-        Call.setRules(Vars.state.rules);
+        if (!Groups.player.isEmpty()) Call.setRules(Vars.state.rules);
     }
 }
